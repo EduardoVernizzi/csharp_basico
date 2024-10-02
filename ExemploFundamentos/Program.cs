2cc219d05058606541fe832e4e2e11bd95816f09
Eduardo Vernizzi <eduardo.vernizzi@hotmail.com>
2024-10-01 21:35:37 -0300

    loops

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,28 +1,87 @@
 ﻿
 using ExemploFundamentos.Models;
 using System.Data;
+using System.Text;
 
 //Introdução a laços de repetição
 
-int numero = 10;
+string opcao;
+bool exibirMenu = true;
 
-
-for (int i = 0; i <= 10; i++)
+while (exibirMenu)
 {
-
-    Console.WriteLine($"{numero} x {i} = {numero*i}");
    
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
 
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1": 
+            Console.WriteLine("\nCadastro de cliente\n");
+            break;
+
+        case "2": 
+            Console.WriteLine("\nBusca de cliente\n");
+            break;
+
+        case "3": 
+            Console.WriteLine("\nApagar cliente\n");
+            break;
+
+        case "4": 
+            Console.WriteLine("\nEncerrar\n");
+            exibirMenu=false;
+            break;
+
+        default: Console.WriteLine("\nOpção inválida!\n");
+            break;
+    }
 }
 
+Console.WriteLine("O programa se encerrou!");
 
 
 
+//int soma = 0, numero = 0;
+
+//do
+//{
+//    Console.WriteLine($"Digite um número (0 para parar): ");
+//    numero = Convert.ToInt32( Console.ReadLine() );
+//    soma += numero;
+
+//} while( numero != 0 );
+
+//Console.WriteLine($"A soma dos números digitados é: {soma}");
 
 
+//int numero = 5;
+//int i = 0;
+
+//while (i <= 10)
+//{
+//    Console.WriteLine($"{numero} x {i} = {numero * i}");
+//    i++;
+
+//    if(i == 6)
+//    {
+//        break;
+//    }
+//}
 
 
+//int numero = 10;
 
+//for (int i = 0; i <= 10; i++)
+//{
+//    Console.WriteLine($"{numero} x {i} = {numero*i}");
+
+//}
 
 
 

98f69f575d357a095a924ba0dd9727b0184b9b8b
Eduardo Vernizzi <eduardo.vernizzi@hotmail.com>
2024-10-03 21:06:21 -0300

    difference in frameworks version

---
+++ ExemploFundamentos.NET6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.NET6
+{
+    internal class Program
+    {
+    }
+}
